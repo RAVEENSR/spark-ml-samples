e2d2cc33b97e612a4a992a4ebcbf17c48a7b7c52
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2022-04-13 01:43:44 +0530

    Read data from csv file

--- spark-driver/src/main/java/com/lohika/morning/ml/spark/driver/service/lyrics/Genre.java
+++ spark-driver/src/main/java/com/lohika/morning/ml/spark/driver/service/lyrics/Genre.java
@@ -2,14 +2,14 @@ package com.lohika.morning.ml.spark.driver.service.lyrics;
 
 public enum Genre {
 
-    METAL("METAL", 0D), // TODO Senthuran This will be the custom genre
+    RAP("RAP", 0D),
     POP("POP", 1D),
     COUNTRY("COUNTRY", 2D),
     BLUES("BLUES", 3D),
     JAZZ("JAZZ", 4D),
     REGGAE("REGGAE", 5D),
     ROCK("ROCK", 6D),
-    HIPHOP("HIPHOP", 7D),
+    HIPHOP("HIP HOP", 7D),
     UNKNOWN("UNKNOWN", -1D);
 
     private final String name;
